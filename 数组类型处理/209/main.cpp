9aa88cdf7e8132d32d0f7648dc583e051e1ee7aa
金皆 <51220436+coderzhongkaikai@users.noreply.github.com>
2020-07-09 10:28:53 +0800

    Update main.cpp

--- 数组类型处理/209/main.cpp
+++ 数组类型处理/209/main.cpp
@@ -3,19 +3,21 @@
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-    int l=0,r=-1;
+         int l=0,r=-1;//-1 为了防止数组溢出
     int sum=0;
-    int res=nums.size()+1;
-    while (l<nums.size(){
+    int res=nums.size()+1;  //+1为了判断 所有数组没有满足题目情况
+    while (l<nums.size()){
         if(r+1<nums.size()&& sum<s)
             sum +=nums[++r];
         else
             sum -=nums[l++];
         if(sum>=s)
             res=min(res,r-l+1);
-        if(res==nums.size()+1)
-            return 0;
-    })
+
+    };
+          if(res==nums.size()+1) return 0; //为了判断 没有满足题目情况的最小数组
+
+      return res;
     }
 };
 
